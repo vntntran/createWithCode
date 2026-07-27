27baf2ac3fffb16e7eae3cff9d21eaa750e7fc5e
Shawny193 <valentin.tran118@gmail.com>
2026-07-27 23:40:40 +0200

    feat(chall): challenge 1 clear

---
+++ Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public InputAction moveAction;
+    private Vector2 verticalInput;
+    public GameObject propeller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveAction.Enable();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = moveAction.ReadValue<Vector2>();
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput.y);
+
+        // Rotate the propeller
+        propeller.transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
